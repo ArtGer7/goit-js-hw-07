bf7f0d4772b25c9668b126f5c1bd1ffeb23947dd
ArtGer7 <118134108+ArtGer7@users.noreply.github.com>
2023-04-15 22:22:42 +0300

    for inspection

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+const gallery = document.querySelector("ul.gallery");
 
+// New structure to add to the initial html document
+const markup = galleryItems.map(({preview, original, description}) => 
+`<li class="gallery__item">
+<a class="gallery__link js-target" href="${original}">
+<img class="gallery__image js-target" src="${preview}" data-source="${original}" alt="${description}">
+</a>
+</li>`);
 
+gallery.insertAdjacentHTML('beforeend', markup.join(""));
+gallery.addEventListener('click', onClick);
+
+function onClick(evt){
+    if (!evt.target.classList.contains("js-target")){
+        return;
+    }
+    console.log(evt.target);
+    evt.preventDefault();
+
+const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${evt.target.dataset.source}" alt="${evt.target.alt}" width="900">
+    </div>
+`)
+
+instance.show();
+document.addEventListener("keydown", onKeyDown);
+
+function onKeyDown({key}){
+if(key === "Escape"){
+    instance.close();
+}
+}}
 
 // Завдання 1 - галерея зображень
 // Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
